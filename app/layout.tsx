01c2d4e4d0280a5a3d61dbbb75a7a1e2b883b293
George Chaitidis <gchaitides@Georges-MacBook-Pro.local>
2025-06-30 12:26:59 +0300

    meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SciGen Offices',
+  description: 'Created with love',
 }
 
 export default function RootLayout({
